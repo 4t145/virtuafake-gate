92da3109032aee77f2738360aeb2c9df86579fc3
4t145 <u4t145@163.com>
2022-11-06 18:46:31 +0800

    move dep to remote

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::sync::{RwLock, Arc};
-use http_api_util::{cache::*, Api};
+use http_api_util::{cache::*};
 use actix_web::{web, App, middleware, HttpServer};
 use bilibili_client::api::user::info::UserInfo;
 use bilibili_client::reqwest_client::FifoRwlCache;
@@ -25,11 +25,12 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         let cors = Cors::default()
-              .allowed_origin("localhost")
-              .allowed_origin("127.0.0.1")
-              .allow_any_origin()
+              .allowed_origin("http://localhost")
+              .allowed_origin("http://127.0.0.1")
+              .allowed_origin("http://vrp.4t145.com")
+              .allowed_origin("https://vrp.4t145.com")
               .allow_any_method()
-              .max_age(3600);
+              .max_age(3500);
 
         App::new().app_data(app_data.clone())
         .wrap(middleware::DefaultHeaders::new().add(("Content-Type", "text/html; charset=utf-8")))
